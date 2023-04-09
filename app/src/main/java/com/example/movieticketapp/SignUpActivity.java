3ccb1729ce2f6cf6eb17cb844afe89f895ef0707
TuongPhamCT <tuongpt12503@gmail.com>
2023-04-09 20:37:21 +0700

    Update SignIn and SignUp activity

--- app/src/main/java/com/example/movieticketapp/SignUpActivity.java
+++ app/src/main/java/com/example/movieticketapp/SignUpActivity.java
@@ -1,13 +1,29 @@
 package com.example.movieticketapp;
 
+import android.app.Instrumentation;
+import android.content.Intent;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class SignUpActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        ImageView imageView =  findViewById(R.id.addimage);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent =  new Intent(Intent.ACTION_PICK);
+                intent.setData(MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+
+            }
+        });
     }
 }
