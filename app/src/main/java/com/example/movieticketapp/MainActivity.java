92d9a0b8c7b65cf6f6ad7457d484cf3c8c0fb814
Ly Thanh Tu Anh <tuanhlythanh@gmail.com>
2023-04-09 14:20:35 +0700

    Update yeu cau lan 1

--- app/src/main/java/com/example/movieticketapp/MainActivity.java
+++ app/src/main/java/com/example/movieticketapp/MainActivity.java
@@ -7,19 +7,10 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button btn = findViewById(R.id.btnToUP);
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getBaseContext(), UserProflingActivity.class);
-                startActivity(intent);
-            }
-        });
     }
 }
