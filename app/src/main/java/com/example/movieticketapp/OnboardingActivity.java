50832526468a57401de7e626774f93c00f95a754
Hồ Đình Mạnh <21522327@gm.uit.edu.vn>
2023-04-10 15:59:35 +0700

    merge Sign In | Sign Up Screen'

--- app/src/main/java/com/example/movieticketapp/OnboardingActivity.java
+++ app/src/main/java/com/example/movieticketapp/OnboardingActivity.java
@@ -1,7 +1,10 @@
 package com.example.movieticketapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 public class OnboardingActivity extends AppCompatActivity {
@@ -13,5 +16,13 @@ public class OnboardingActivity extends AppCompatActivity {
         getSupportActionBar().hide();
         //ImageView logo = findViewById(R.id.logo);
         //logo.setImageResource(R.drawable.splash_logo);
+        Button getStartBt = findViewById(R.id.button);
+        getStartBt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(),SignInActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
