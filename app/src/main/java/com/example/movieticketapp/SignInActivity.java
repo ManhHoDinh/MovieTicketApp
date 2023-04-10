bf0aa8b7ed68876c4b9a325034599ad03db23628
Hồ Đình Mạnh <21522327@gm.uit.edu.vn>
2023-04-10 15:43:24 +0700

    Update sign In | Sign Up

--- app/src/main/java/com/example/movieticketapp/SignInActivity.java
+++ app/src/main/java/com/example/movieticketapp/SignInActivity.java
@@ -1,7 +1,10 @@
 package com.example.movieticketapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class SignInActivity extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class SignInActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+        TextView signUp = findViewById(R.id.SignUp);
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(),SignUpActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/movieticketapp/SignUpActivity.java
+++ app/src/main/java/com/example/movieticketapp/SignUpActivity.java
@@ -7,6 +7,7 @@ import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 public class SignUpActivity extends AppCompatActivity {
@@ -22,7 +23,20 @@ public class SignUpActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Intent intent =  new Intent(Intent.ACTION_PICK);
                 intent.setData(MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-
+            }
+        });
+        Button backBt = findViewById(R.id.backbutton);
+        backBt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+        Button signUpBt =  findViewById(R.id.SignUpBtn);
+        signUpBt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
             }
         });
     }
