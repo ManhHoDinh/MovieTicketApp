644dac385de25546452c1c3667c40c637cf8cca1
TuongPhamCT <tuongpt12503@gmail.com>
2023-04-17 15:30:30 +0700

    Package My Ticket

---
+++ app/src/main/java/com/example/movieticketapp/Activity/MyTicketExpired.java
@@ -0,0 +1,30 @@
+package com.example.movieticketapp.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ListView;
+
+import com.example.movieticketapp.Adapter.TicketListAdapter;
+import com.example.movieticketapp.Model.Ticket;
+import com.example.movieticketapp.R;
+
+import java.util.ArrayList;
+
+public class MyTicketExpired extends AppCompatActivity {
+    ListView listView;
+    ArrayList<Ticket> arrayList = new ArrayList<Ticket>();
+    TicketListAdapter adapter;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_ticket_expired);
+        listView = (ListView) findViewById(R.id.ListViewTicket3);
+
+        arrayList.add(new Ticket("Ralph Breaks the Internet 1", "16:40, Sun May 22", "FX Sudirman XXI", R.drawable.poster_ralph));
+        arrayList.add(new Ticket("Ralph Breaks the Internet 2", "16:40, Sun May 22", "FX Sudirman XXI", R.drawable.poster_ralph));
+
+        adapter = new TicketListAdapter(getApplicationContext(), R.layout.list_ticket_view, arrayList);
+        listView.setAdapter(adapter);
+    }
+}
