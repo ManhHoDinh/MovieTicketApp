7ea9c0b56da2756973f709bcbec1b56ac87f0c0d
TuongPhamCT <tuongpt12503@gmail.com>
2023-05-24 16:44:55 +0700

    add movies function

--- app/src/main/java/com/example/movieticketapp/Activity/Movie/AddMovieActivity.java
+++ app/src/main/java/com/example/movieticketapp/Activity/Movie/AddMovieActivity.java
@@ -11,6 +11,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -21,13 +22,32 @@ import com.example.movieticketapp.Activity.Ticket.MyTicketAllActivity;
 import com.example.movieticketapp.Activity.Wallet.MyWalletActivity;
 import com.example.movieticketapp.R;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.protobuf.Any;
 import com.makeramen.roundedimageview.RoundedImageView;
 
 import org.w3c.dom.Text;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class AddMovieActivity extends AppCompatActivity {
     private BottomNavigationView bottomNavigationView;
     int th;
+    StorageReference storageReference = FirebaseStorage.getInstance().getReference();
+    FirebaseFirestore databaseReference = FirebaseFirestore.getInstance();
+    DocumentReference document;
+    EditText description;
+    EditText movieName;
+    EditText movieKind;
+    EditText movieDurarion;
+    EditText movieStatus;
+    Button applyButton;
+    Button cancleButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -50,6 +70,7 @@ public class AddMovieActivity extends AppCompatActivity {
             }
             return true;
         });
+
         ImageView moviebackground = (ImageView) findViewById(R.id.moviebackground);
         TextView textbg = (TextView) findViewById(R.id.textbackground);
         ImageView imbg = (ImageView) findViewById(R.id.imbackground);
@@ -58,7 +79,12 @@ public class AddMovieActivity extends AppCompatActivity {
         TextView textavt = (TextView) findViewById(R.id.textavt);
         ImageView imavt = (ImageView) findViewById(R.id.imavt);
 
-        EditText description = (EditText) findViewById(R.id.moviedes);
+        description = (EditText) findViewById(R.id.moviedes);
+        movieName = (EditText) findViewById(R.id.movieName);
+        movieKind = (EditText) findViewById(R.id.movieKind);
+        movieDurarion =(EditText) findViewById(R.id.movieDuration);
+        movieStatus = (EditText) findViewById(R.id.moviestatus);
+        applyButton = (Button) findViewById(R.id.applybutton);
 
         RoundedImageView movieactor = (RoundedImageView) findViewById(R.id.movieactor);
         ImageView imcast = (ImageView) findViewById(R.id.imcast);
@@ -137,7 +163,23 @@ public class AddMovieActivity extends AppCompatActivity {
                 th = 3;
                 texttrailer.setText("");
                 imtrailer.setImageResource(0);
-                 
+            }
+        });
+        applyButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                document = databaseReference.document("Movies/PhamThanhTuong");
+                HashMap<String, Any> data = new HashMap<String, Any>();
+                data["BackGroudImage"] = movieName.getText().toString();
+                document.set("PosterImage");
+                document.set("PrimaryImage");
+                document.set("description");
+                document.set("durationTime");
+                document.set("genre");
+                document.set("id");
+                document.set("name");
+                document.set("status");
+                document.set("vote");
             }
         });
     }
