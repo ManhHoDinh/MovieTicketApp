648b22afda5f8796526c522782d4030027596315
vocongbinh <105732042+vocongbinh@users.noreply.github.com>
2023-06-06 16:43:13 +0700

    dev: no internet screen

--- app/src/main/java/com/example/movieticketapp/NetworkChangeListener.java
+++ app/src/main/java/com/example/movieticketapp/NetworkChangeListener.java
@@ -1,37 +1,50 @@
 package com.example.movieticketapp;
 
 import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.widget.AppCompatButton;
+
+import com.example.movieticketapp.Activity.Account.SignInActivity;
 
 public class NetworkChangeListener extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
-        if(!Common.isConnectedToInternet(context)){
-            AlertDialog.Builder builder = new AlertDialog.Builder(context);
-            View layout_dialog = LayoutInflater.from(context).inflate(R.layout.check_internet_dialog, null);
-            builder.setView(layout_dialog);
-            Button retryBtn = layout_dialog.findViewById(R.id.retryBtn);
-            AlertDialog dialog = builder.create();
-            dialog.show();
-            dialog.setCancelable(false);
-            dialog.getWindow().setGravity(Gravity.CENTER);
-            retryBtn.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    dialog.dismiss();
-                    onReceive(context, intent);
-                }
-            });
+        if (!Common.isConnectedToInternet(context)) {
+            Dialog layout_dialog = new Dialog(context);
+            layout_dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+            layout_dialog.setContentView(R.layout.check_internet_dialog);
+            Window window = layout_dialog.getWindow();
+            if (window != null) {
+                window.setLayout(WindowManager.LayoutParams.MATCH_PARENT, WindowManager.LayoutParams.MATCH_PARENT);
+                window.setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                WindowManager.LayoutParams windowAttribute = window.getAttributes();
+                windowAttribute.gravity = Gravity.CENTER;
+                window.setAttributes(windowAttribute);
+                layout_dialog.show();
+                Button retryBtn = layout_dialog.findViewById(R.id.retryBtn);
+                retryBtn.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        layout_dialog.dismiss();
+                        onReceive(context, intent);
+                    }
+                });
 
 
+            }
         }
     }
 }
