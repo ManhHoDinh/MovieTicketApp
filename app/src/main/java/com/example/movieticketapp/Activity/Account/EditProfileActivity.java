e22dd091fe96baa3e615bb4329a052ce14b5647a
TuongPhamCT <tuongpt12503@gmail.com>
2023-07-23 23:47:19 +0700

    update avatar

--- app/src/main/java/com/example/movieticketapp/Activity/Account/EditProfileActivity.java
+++ app/src/main/java/com/example/movieticketapp/Activity/Account/EditProfileActivity.java
@@ -217,7 +217,7 @@ public class EditProfileActivity extends AppCompatActivity {
         });
         FirebaseRequest.database.collection("Users").document(currentUser.getUid()).update("Email", emailET.getText().toString());
     }
-//    void SuccessUpdate()
+    //    void SuccessUpdate()
 //    {
 //        Intent i = new Intent(EditProfileActivity.this, AccountActivity.class);
 //        startActivity(i);
@@ -255,13 +255,11 @@ public class EditProfileActivity extends AppCompatActivity {
                         FirebaseStorage.getInstance().getReferenceFromUrl(cinemaImg).getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
                             @Override
                             public void onSuccess(Uri uri) {
-                                Log.e("fd", uri.toString());
                                 UserProfileChangeRequest userProfileChangeRequest = new UserProfileChangeRequest.Builder()
                                         .setPhotoUri(uri)
                                         .build();
                                 FirebaseAuth.getInstance().getCurrentUser().updateProfile(userProfileChangeRequest);
 
-                                Log.e("fdd", currentUser.getPhotoUrl().toString());
                             }
                         });
                     }
